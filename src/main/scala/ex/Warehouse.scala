c3c7c83113b9e3f53f51d6d0666da5724c9c3584
Emanuele Artegiani <emanuele.artegiani@studio.unibo.it>
2024-04-10 11:55:49 +0200

    Completed ex 2

--- src/main/scala/ex/Warehouse.scala
+++ src/main/scala/ex/Warehouse.scala
@@ -2,13 +2,17 @@ package ex
 
 import util.Optionals.Optional
 import util.Sequences.*
+
 trait Item:
   def code: Int
   def name: String
   def tags: Sequence[String]
 
+case class ItemImpl(code: Int, name: String, tags: Sequence[String]) extends Item
+
 object Item:
-  def apply(code: Int, name: String, tags: Sequence[String] = Sequence.empty): Item = ???
+  // def apply(code: Int, name: String, tags: Sequence[String] = Sequence.empty): Item = ItemImpl(code, name, tags)
+  def apply(code: Int, name: String, tags: String*): Item = ItemImpl(code, name, Sequence(tags: _*))
 
 /**
  * A warehouse is a place where items are stored.
@@ -44,27 +48,59 @@ trait Warehouse:
   def contains(itemCode: Int): Boolean
 end Warehouse
 
+class WarehouseImpl extends Warehouse:
+  private var items: Sequence[Item] = Sequence.empty
+
+  override def store(item: Item): Unit =
+    items = items.concat(Sequence(item))
+
+  override def contains(itemCode: Int): Boolean =
+//    items.contains(items.filter(_.code == itemCode).head.orElse(null))
+    items.contains(items.find(_.code == itemCode).orElse(null))
+
+  override def searchItems(tag: String): Sequence[Item] =
+    items.filter(_.tags.contains(tag))
+
+  override def retrieve(code: Int): Optional[Item] =
+    items.find(_.code == code)
+
+  override def remove(item: Item): Unit =
+    items = items.filter(_ != item)
+
+
 object Warehouse:
-  def apply(): Warehouse = ???
+  def apply(): Warehouse = new WarehouseImpl
 
 @main def mainWarehouse(): Unit =
   val warehouse = Warehouse()
 
-  val dellXps = Item(33, "Dell XPS 15", Sequence("notebook"))
-  val dellInspiron = Item(34, "Dell Inspiron 13", Sequence("notebook"))
-  val xiaomiMoped = Item(35, "Xiaomi S1", Sequence("moped", "mobility"))
+//  val dellXps = Item(33, "Dell XPS 15", Sequence("notebook"))
+//  val dellInspiron = Item(34, "Dell Inspiron 13", Sequence("notebook"))
+//  val xiaomiMoped = Item(35, "Xiaomi S1", Sequence("moped", "mobility"))
+  val dellXps = Item(33, "Dell XPS 15", "notebook")
+  val dellInspiron = Item(34, "Dell Inspiron 13", "notebook")
+  val xiaomiMoped = Item(35, "Xiaomi S1", "moped", "mobility")
 
-  warehouse.contains(dellXps.code) // false
+  // warehouse.contains(dellXps.code) // false
+  assert(!warehouse.contains(dellXps.code), "Test 1 failed")
   warehouse.store(dellXps) // side effect, add dell xps to the warehouse
-  warehouse.contains(dellXps.code) // true
+  assert(warehouse.contains(dellXps.code), "Test 2 failed")
+  // warehouse.contains(dellXps.code) // true
+  assert(warehouse.contains(dellXps.code), "Test 3 failed")
   warehouse.store(dellInspiron) // side effect, add dell Inspiron to the warehouse
   warehouse.store(xiaomiMoped) // side effect, add xiaomi moped to the warehouse
-  warehouse.searchItems("mobility") // Sequence(xiaomiMoped)
-  warehouse.searchItems("notebook") // Sequence(dellXps, dell Inspiron)
-  warehouse.retrieve(11) // None
-  warehouse.retrieve(dellXps.code) // Just(dellXps)
+  assert(warehouse.contains(dellInspiron.code) && warehouse.contains(xiaomiMoped.code), "Test 4 failed")
+  // warehouse.searchItems("mobility") // Sequence(xiaomiMoped)
+  assert(warehouse.searchItems("mobility") == Sequence(xiaomiMoped), "Test 5 failed")
+  // warehouse.searchItems("notebook") // Sequence(dellXps, dell Inspiron)
+  assert(warehouse.searchItems("notebook") == Sequence(dellXps, dellInspiron), "Test 6 failed")
+  // warehouse.retrieve(11) // None
+  assert(warehouse.retrieve(11).isEmpty, "Test 7 failed")
+  // warehouse.retrieve(dellXps.code) // Just(dellXps)
+  assert(warehouse.retrieve(dellXps.code) == Optional.Just(dellXps), "Test 8 failed")
   warehouse.remove(dellXps) // side effect, remove dell xps from the warehouse
-  warehouse.retrieve(dellXps.code) // None
+  // warehouse.retrieve(dellXps.code) // None
+  assert(warehouse.retrieve(dellXps.code).isEmpty, "Test 9 failed")
 
 /** Hints:
  * - Implement the Item with a simple case class
